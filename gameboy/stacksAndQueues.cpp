e0d3ea70a85a1241458a7687625b6610c5957b78
jc <joshua.coutinho1@hotmail.com>
2023-10-22 14:30:14 +0100

    Working on new sound implementation

--- gameboy/stacksAndQueues.cpp
+++ gameboy/stacksAndQueues.cpp
@@ -9,70 +9,243 @@
 #include <map>
 #include <algorithm>
 #include <bitset>
+#include <algorithm>
+#include <cmath>
+#include <array>
 
 using namespace std;
 
-class Fancy {
+#include <stdio.h>
+#include <cstdlib>
+#include <vector>
+#include <deque>
+#include <map>
+#include <cstdint>
+#include <cassert>
+
+/*
+    OrderBook is a level 3 orderbook.  Please fill out the stub below.
+*/
+
+using u64 = uint64_t;
+using i64 = int64_t;
+
+i64 PRECISION = 1e9;
+
+struct Order {
+    const int orderId;
+    const char side;
+    int size;
+    i64 priceL;
+
+    Order(int orderId, char side, int size, i64 price) : orderId{orderId}, side{side}, size{size}, priceL{price} {}
+
+
+};
+
+using OrderId = int;
+
+
+using L2 = map<OrderId, Order>;
+using SideLevels = deque<L2>;
+
+struct OrderIdLoc {
+    L2 &l2;
+    L2::iterator loc;
+
+    OrderIdLoc(L2 &l2, L2::iterator loc) : l2{l2}, loc{loc} {
+    }
+
+};
+
+class OrderBook {
+    map<OrderId, OrderIdLoc> orderIdMap;
+    SideLevels bidLevels;
+    SideLevels askLevels;
 
 public:
 
-    vector<long long> vals;
-    vector<long long> prods;
-    vector<long long> sums;
-    constexpr static long long mod = 1e9 + 7;
-
-    Fancy() {
-        sums.push_back(0);
-        prods.push_back(1);
+    OrderBook() : bidLevels(), askLevels(), orderIdMap() {
     }
 
-    void append(int val) {
-        vals.push_back(val);
-        *sums.rbegin() = (*sums.rbegin() * *prods.rbegin()) % mod;
-        sums.push_back(*sums.rbegin());
-        prods.push_back((*prods.rbegin()));
+    static i64 getPriceL(const L2 &bids) {
+        assert(!bids.empty());
+        return bids.begin()->second.priceL;
     }
 
-    void addAll(int inc) {
-        *sums.rbegin() = ((*sums.rbegin() * *prods.rbegin())%mod + inc) % mod;
+    static double getPriceD(const L2 &bids) {
+        assert(!bids.empty());
+        return bids.begin()->second.priceL / double(PRECISION);
     }
 
-    void multAll(int m) {
-        *prods.rbegin() = (*prods.rbegin() * m) % mod;
+    static bool isAggressiveOrder(i64 price, SideLevels &levels, char side) {
+        int dir = side == 'b' ? 1 : -1;
+        return !levels.empty() && (getPriceL(levels[0]) - price) * dir < 0;
     }
 
-    long long findInverse(long long x, long long y) {
-        long long z = 1;
-        while (x % y != 0) {
-            z += (mod - y) / y + 2;
-            y = y - (mod - y) % y;
+    static i64 normPrice(double price) {
+        return i64(PRECISION * price);
+    }
+
+    bool isBid(char side) {
+        assert(side == 'b' || side == 's');
+        return side == 'b';
+    }
+
+    void insertLevel(int orderId, char side, int size, i64 priceL) {
+
+        SideLevels &levels = side == 'b' ? bidLevels : askLevels;
+
+        auto compare = [side](i64 a, i64 b) {
+            if (side == 'b') {
+                return a > b;
+            } else {
+                return a < b;
+            }
+        };
+        auto iter = levels.begin();
+        for (; iter != levels.end(); ++iter) {
+            i64 curPrice = getPriceL(*iter);
+            if (compare(priceL, curPrice)) {
+                L2 mp{{orderId, Order{orderId, side, size, priceL}}};
+
+                auto insertPos = levels.emplace(iter, mp);
+                auto res = insertPos->find(orderId);
+                orderIdMap.emplace(orderId, OrderIdLoc{*iter, res});
+            } else if (priceL == curPrice) {
+                auto res = iter->emplace(orderId, Order{orderId, side, size, priceL});
+                if (res.second) {
+                    assert(false);
+                }
+                orderIdMap.emplace(orderId, OrderIdLoc{*iter, res.first});
+            }
         }
-        return z + x / y - 1;
+
+        assert(iter == levels.end());
+        L2 mp{{orderId, Order{orderId, side, size, priceL}}};
+        const deque<L2>::iterator &insertPos = levels.emplace(iter, mp);
+        const auto& res = insertPos->find(orderId);
+        orderIdMap.emplace(orderId, OrderIdLoc{*insertPos, res});
 
     }
 
-    int getIndex(int idx) {
-        long long prod = findInverse(*prods.rbegin(), prods[idx]);
-        long long inc = (*sums.rbegin() < sums[idx]) ? 1e9 + 7 : 0;
-        long long sum = (((inc + *sums.rbegin()) - sums[idx])*prod) % mod;
-        return ((prod * vals[idx]) % mod + sum) % mod;
+    //adds order to order book
+    void newOrder(int orderId, char side, int size, double priceD) {
+        SideLevels &levelsOpp = getOppSideLevels(side);
+        i64 priceL = normPrice(priceD);
+        if (isAggressiveOrder(priceL, levelsOpp, side)) {
+            assert(false);
+        } else {
+            insertLevel(orderId, side, size, priceL);
+        }
+    }
+
+    void modifyOrder(int oldOrderId, int orderId, char side, int newSize, double newPrice) {
+    }
+
+    //replaces order with different order
+    void reduceOrder(int orderId, int size) {
+        const auto &elem = orderIdMap.find(orderId);
+        assert(elem != orderIdMap.end());
+        auto &pos = elem->second;
+        assert(pos.loc->second.size >= size);
+        pos.loc->second.size = size;
+        if (pos.loc->second.size <= 0) {
+            pos.l2.erase(pos.loc);
+        }
+    }
+
+    //deletes order from orderbook
+    void deleteOrder(int orderId) {
+        const auto &elem = orderIdMap.find(orderId);
+        assert(elem != orderIdMap.end());
+
+        elem->second.l2.erase(elem->second.loc);
+    }
+
+
+    //returns the number of levels on the side
+    int getNumLevels(char side) {
+        return int(getSideLevels(side).size());
+    }
+
+    //returns the price of a level.  Level 0 is top of book.
+    double getLevelPrice(char side, int level) {
+        return getPriceD(getSideLevels(side)[level]);
+    }
+
+    //returns the size of a level.
+    int getLevelSize(char side, int level) {
+        // TODO - optimize this.
+        auto mp = getSideLevels(side)[level];
+        int res = 0;
+        for (const auto &e: mp) {
+            res += e.second.size;
+        }
+        return res;
+    }
+
+    //returns the number of orders contained in price level.
+    int getLevelOrderCount(char side, int level) {
+        return int(getSideLevels(side)[level].size());
+    }
+
+    SideLevels &getSideLevels(char side) {
+        return side == 'b' ? bidLevels : askLevels;
+    }
+
+    SideLevels &getOppSideLevels(char side) {
+        return side == 'b' ? askLevels : bidLevels;
     }
 };
 
-int main() {
-    Fancy f;
-    int vals_[] = {2, 3, 7, 2, 0, 3, 10, 2, 0, 1, 2};
-    int *vals = vals_;
-    f.append(2);
-    f.addAll(3);
-    f.append(7);
-    f.multAll(2);
-    cout << f.getIndex(0) << endl;
-    f.addAll(3);
-    f.append(10);
-    f.multAll(2);
-    cout << f.getIndex(0) << endl;
-    cout << f.getIndex(1) << endl;
-    cout << f.getIndex(2) << endl;
+/*
+    Do not change main function
+*/
 
+int main(int argc, char *argv[]) {
+    char instruction;
+    char side;
+    int orderId;
+    double price;
+    int size;
+    int oldOrderId;
+
+//    FILE *file = fopen(std::getenv("OUTPUT_PATH"), "w");
+    FILE *file = fopen("/home/jc/CLionProjects/gb_emulator/data/output.txt", "w");
+
+    OrderBook book;
+
+    while (scanf("%c %c %d %d %lf %d\n", &instruction, &side, &orderId, &size, &price, &oldOrderId) != EOF) {
+        switch (instruction) {
+            case 'n':
+                book.newOrder(orderId, side, size, price);
+                break;
+            case 'r':
+                book.reduceOrder(orderId, size);
+                break;
+            case 'm':
+                book.modifyOrder(oldOrderId, orderId, side, size, price);
+                break;
+            case 'd':
+                book.deleteOrder(orderId);
+                break;
+            case 'p':
+                fprintf(file, "%.02lf\n", book.getLevelPrice(side, orderId));
+                break;
+            case 's':
+                fprintf(file, "%d\n", book.getLevelSize(side, orderId));
+                break;
+            case 'l':
+                fprintf(file, "%d\n", book.getNumLevels(side));
+                break;
+            case 'c':
+                fprintf(file, "%d\n", book.getLevelOrderCount(side, orderId));
+                break;
+            default:
+                fprintf(file, "invalid input\n");
+        }
+    }
+    fclose(file);
+    return 0;
 }
